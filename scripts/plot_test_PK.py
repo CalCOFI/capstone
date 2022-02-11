de2cf8c948d0c1b9c5f5a0ed16dd41cda7dabbef
PK0207 <30590837+PK0207@users.noreply.github.com>
2022-02-11 11:20:30 -0800

    plotting and data organization tests

--- scripts/plot_test_PK.py
+++ scripts/plot_test_PK.py
@@ -16,7 +16,7 @@ import plotly.express as px
 #%%
 #setup
 
-data = pd.read_csv('bottle_and_cast.csv')
+data = pd.read_csv('data/bottle_and_cast.csv')
 geometry = [Point(xy) for xy in zip(data['Lat_Dec'].values,data['Lon_Dec'].values)]
 gdf = GeoDataFrame(data, geometry = geometry)
 
@@ -26,6 +26,9 @@ world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
 gdf.plot(ax=world.plot(figsize=(15, 15)), marker='o', color='red', markersize=15)
 #%%
 #plotly plot
-fig = px.scatter_geo(data, lat = 'Lat_Dec',lon = 'Lon_Dec', hover_name = "Depthm")#hover_name can be list
+#first make a new dataframe
+d = {'lat':data['Lat_Dec'].unique,'lon':data['Lon_Dec'].unique}
+df = pd.DataFrame(data = d)
+fig = px.scatter_geo(data, lat = data['Lat_Dec'].unique,lon = data['Lon_Dec'].unique, hover_name = "Depthm")#hover_name can be list
 fig.update_layout(title = 'Title', title_x=0.5)
 fig.show()
