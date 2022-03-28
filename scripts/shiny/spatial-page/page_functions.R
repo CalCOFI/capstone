a14669eb6f89a76438e3e999de2e5b2a428a7abf
mgupta701 <mallika.gupta104@gmail.com>
2022-03-28 15:53:46 -0700

    Functionalizing station line profile plots
    Renamed quarters to seasons, created make_station_line function in page_functions.R

--- scripts/shiny/spatial-page/page_functions.R
+++ scripts/shiny/spatial-page/page_functions.R
@@ -124,6 +124,47 @@ make_profile <- function(yr, qr){
           axis.text.y = element_text(size = 8))
 }
 
+make_station_line <- function(yr, lin){
+  bottle %>%
+    # filter to year, quarter, and station of interest
+    filter(year == yr, 
+           # quarter == qr,
+           line == lin) %>%
+    # bin depths into roughly even numbers of observations
+    mutate(depth_interval = cut_number(depth, 10)) %>%
+    # aggregate within depth bins
+    mutate(quarter = replace(quarter, quarter == 1, "Q1 - Winter")) %>% 
+    mutate(quarter = replace(quarter, quarter == 2, "Q2 - Spring")) %>% 
+    mutate(quarter = replace(quarter, quarter == 3, "Q3 - Summer")) %>%
+    mutate(quarter = replace(quarter, quarter == 4, "Q4 - Fall")) %>%
+    group_by(depth_interval,
+             quarter,
+             distance) %>%
+    summarize(oxygen = median(oxygen, na.rm = T)) %>% # tinker with summary stat
+    ggplot(aes(x = distance, y = fct_rev(depth_interval))) +
+    facet_wrap(~ quarter, 
+               # scales = "free_x",
+               nrow = 4) +
+    # using geom_tile instead of raster in order to create boxes of different widths
+    geom_tile(aes(fill = oxygen, width = distance)) +
+    # adjust color scale
+    scale_fill_gradient2(low = '#E74C3C',
+                         mid = '#F1C40F',
+                         high = '#3498DB',
+                         midpoint = log10(1.4),
+                         trans = 'log10') +
+    # aesthetics
+    theme_minimal() +
+    theme(axis.text.x = element_text(angle = 90, 
+                                     size = 8,
+                                     vjust = 0.5),
+          panel.grid = element_blank()) +
+    labs(x = 'Distance from shore (Nautical Miles)', y = 'Depth (m)',
+         fill='Oxygen (mL O2/L seawater)') 
+}
+
+
+
 save(
   list = ls(),
   file = 'scripts/shiny/spatial-page/page_functions.RData'
