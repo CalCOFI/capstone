ebebfc255b36cb329b11e6092567c0329013357e
PK0207 <30590837+PK0207@users.noreply.github.com>
2022-04-04 17:30:38 -0700

    Added station line plots
    Added station line plots and the associated lines input. More thought needed on how to optimize line input so that only lines that have been sampled show up as input options.

--- scripts/shiny/app.R
+++ scripts/shiny/app.R
@@ -20,11 +20,12 @@ ui <- navbarPage("CalCOFI", id="nav",
                           includeScript("gomap.js")
                         ),
                         # If not using custom CSS, set height of leafletOutput to a number instead of percent
-                        leafletOutput("map", width="100%", height="100%"),
+                          leafletOutput("map", width="100%", height="100%"),
+                        
                         
                         # Shiny versions prior to 0.11 should use class = "modal" instead.
                         absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-                                      draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+                                      draggable = TRUE, top = 60, left = "auto", right = 700, bottom = "auto",
                                       width = 330, height = "auto",
                                       
                                       h2("Inputs"),
@@ -40,13 +41,25 @@ ui <- navbarPage("CalCOFI", id="nav",
                                                    max = 4,
                                                    step = 1,
                                                    value = 1),
+                                      selectInput('lin',
+                                                  'Line ID',
+                                                  lines,
+                                      ),
 
-                                      #plotOutput('profile'),
+                        ),
+                        column(4,
+                               absolutePanel(id = "controls",class = "panel panel-default",
+                                             fixed = TRUE,
+                                             draggable = FALSE, top = 60, left = "auto", right = 20, bottom = "auto",
+                                             width = 500, height = "auto", 
+                                             h2('Plots'),
+                                             plotOutput("profile"),
+                                             plotOutput('stationline')
+                               )
                         ),
                         
-                        
                         tags$div(id="cite",
-                                 'Data compiled for ', tags$em('Coming Apart: The State of White America, 1960–2010'), ' by Charles Murray (Crown Forum, 2012).'
+                                 'Data compiled for ', tags$em('CalCOFI'), ' by Us'
                         )
                     ),
            ),
@@ -55,11 +68,6 @@ ui <- navbarPage("CalCOFI", id="nav",
                     fluidRow(h2("Something else"))
            ),
            conditionalPanel("false", icon("crosshair")),
-           sidebarPanel(h2("Plots"),
-               border = "bottom",
-               width = 4,
-               plotOutput('profile'),
-               )
 )
 # # user input panel for spatial tab
 # spatial_sidebar <- sidebarPanel(
@@ -165,9 +173,13 @@ server <- function(input, output, session) {
     profile_plot <- reactive({
         make_profile(input$yr, input$qr)
     })
+    profile_plot <- reactive({
+      make_station_line(input$yr, input$lin)
+    })
     
     # plot depth profiles
     output$profile <- renderPlot({profile_plot()})
+    output$stationline <- renderPlot({station_line_plot})
     
 } 
 
