f835f9982281d87ac736cbbcf17981a3e0ed7e2c
PK0207 <30590837+PK0207@users.noreply.github.com>
2022-04-22 13:37:36 -0700

    Started a new draft for a depth_avg_plot
    attempted to make a new page for timeseries plots, didn't work because the map doesn't show up

--- scripts/shiny/app.R
+++ scripts/shiny/app.R
@@ -20,10 +20,8 @@ ui <- navbarPage("CalCOFI", id="nav",
                           includeScript("gomap.js")
                         ),
                         # If not using custom CSS, set height of leafletOutput to a number instead of percent
-                          leafletOutput("map", width="100%", height="100%"),
+                          leafletOutput("map1", width="100%", height="100%"),
                         
-                        
-                        # Shiny versions prior to 0.11 should use class = "modal" instead.
                         absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
                                       draggable = TRUE, top = 60, left = "auto", right = 700, bottom = "auto",
                                       width = 330, height = "auto",
@@ -77,91 +75,10 @@ ui <- navbarPage("CalCOFI", id="nav",
                     ),
            ),
            
-           tabPanel("Something else",
-                    fluidRow(h2("Something else"))
+           tabPanel("Map Animation",
+                    leafletOutput("map", width="100%", height="100%"),
            ),
-           conditionalPanel("false", icon("crosshair")),
 )
-# # user input panel for spatial tab
-# spatial_sidebar <- sidebarPanel(
-#     numericInput('yr', # selection gets stored as `input$yr`
-#                  'Year', 
-#                  min = min(year(bottle$date)),
-#                  max = max(year(bottle$date)),
-#                  value = median(year(bottle$date)),
-#                  step = 1),
-#     numericInput('qr', # selection gets stored as `input$qr`
-#                  'Quarter',
-#                  min = 1,
-#                  max = 4,
-#                  step = 1,
-#                  value = 1),
-#     width = 12)
-# 
-# map_comments <- 'Note: point size shows location variability across all sampling events, but not to scale.'
-# 
-# # define layout for body of spatial tab
-# spatial_tab <- tabItem(tabName = 'spatial',
-#                        fluidRow(
-#                            # user selections
-#                            box(spatial_sidebar,
-#                                title = 'Select time',
-#                                status = 'info',
-#                                solidHeader = T,
-#                                # collapsible = T,
-#                                width = 2),
-#                            # map
-#                            box(map_comments,
-#                                leafletOutput('map', 'width=100%', 'height=100%'),
-#                                title = 'Sampling locations',
-#                                status = 'primary',
-#                                solidHeader = T,),
-#                            # depth profile
-#                            tabBox(
-#                                tabPanel("Profile",
-#                                plotOutput('profile'),
-#                                title = 'Depth profiles',
-#                                status = 'primary',
-#                                solidHeader = T,
-#                                # collapsible = T,
-#                                width = 4),
-#                                tabPanel("Intro",
-#                                         solidHeader = T,
-#                                         # collapsible = T,
-#                                         width = 4),
-#                                width = 4)
-#                        )
-# )
-
-## ------------------------
-## SOME OTHER PAGE
-# 
-# blank_tab <- tabItem(tabName = 'somethingelse',
-#                      h2('Something completely different')) 
-# 
-# ## ------------------------
-# ## USER INTERFACE LAYOUT
-# 
-# # menu to navigate tabs (note tabName must match tabItems)
-# sidebar <- sidebarPanel(sidebarMenu(
-#     menuItem('Spatial variation', tabName = 'spatial'),
-#     menuItem('Another tab', tabName = 'somethingelse')),
-#     width = 1)
-# 
-# # body for each tab
-# body <- mainPanel(
-#     tabItems(
-#         spatial_tab,
-#         blank_tab
-#     )
-# )
-# 
-# # define user interface
-# ui <- fluidPage(
-#     titlePanel("Draft"),
-#     sidebar,
-#     body,
-# )
 
 
 ## ------------------
@@ -173,14 +90,21 @@ server <- function(input, output, session) {
     map_data <- reactive({get_map_data(input$yr, input$qr)})
     
     # base map layer (will show default year 2000)
-    output$map <- renderLeaflet({make_basemap()})
+    output$map1 <- renderLeaflet({make_basemap()})
+    output$map2 <- renderLeaflet({make_basemap()})
     
     # plot user's selection
     observe({
-        leafletProxy('map') %>%
+      input$nav
+      
+        tab1 <- leafletProxy('map1') %>%
             update_basemap(map_data())
-    })
+        tab2 <- leafletProxy('map2') %>%
+          update_basemap(map_data())
+    }
+    )
     
+
     ## PROFILE PANEL
     # user retrieve data by year/quarter
     profile_plot <- reactive({
