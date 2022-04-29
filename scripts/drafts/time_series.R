6277e0e88e9f31f8b1dcecb2bd505361d60bbfb8
dcoletta9 <dcoletta@ucsb.edu>
2022-04-29 13:28:51 -0700

    Update time series plots

--- scripts/drafts/time_series.R
+++ scripts/drafts/time_series.R
@@ -3,22 +3,79 @@ library(htmltools)
 library(tidyverse)
 library(lubridate)
 library(ggplot2)
+library(plyr)
+library(dplyr)
+library(hrbrthemes)
 load("data/processed/bottle.RData")
 
 
-bot0<- subset(bottle, depth>=0 & depth <= 15)
-dates<-as.data.frame(bot0$date)
+sunbot<- subset(bottle, depth>=0 & depth <= 200)
+twibot <- subset(bottle, depth>200 & depth<=1000)
+dates<-as.data.frame(sunbot$date)
 
-tp <- ggplot(dates, aes(x=bot0$date)) +
-  geom_line(aes(y=bot0$temperature), color = "darkred", na.rm = TRUE) + 
-  xlab("Date")
+#geom_line(aes(y=twibot$temperature),color = "steelblue", na.rm = TRUE)
 
-tp+scale_x_date(limit=c(as.Date("2000-01-01"),as.Date("2000-01-30")),
-                date_labels = "%Y %b %d")
+# data_summary <- function(data, varname, groupnames){
+#   require(plyr)
+#   summary_func <- function(x, col){
+#     c(mean = mean(x[[col]], na.rm=TRUE),
+#       sd = sd(x[[col]], na.rm=TRUE))
+#   }
+#   data_sum<-ddply(data, groupnames, .fun=summary_func,
+#                   varname)
+#   data_sum <- rename(data_sum, c("mean" = varname))
+#   return(data_sum)
+# }
+# 
+# df3 <- data_summary(sunbot, varname="temperature", 
+#                     groupnames=c("depth", "date"))
 
-op <- ggplot(dates, aes(x=bot0$date)) +
-  geom_line(aes(y=bot0$oxygen), color = "blue", na.rm = TRUE) + 
-  xlab("Date")
+#geom_line(aes(lineteype=depth),na.rm = TRUE) +
+  #geom_point(aes(color=depth))
+#geom_errorbar(aes(ymin=temperature-sd, ymax = temperature+sd, width = .1))
 
-op+scale_x_date(limit=c(as.Date("2000-01-01"),as.Date("2000-07-01")),
-                date_labels = "%Y %b %d")
+tps <- ggplot(sunbot, aes(date,temperature, group = depth)) +
+  geom_line(na.rm = TRUE) +
+  geom_point(aes(color=depth)) +
+  xlab("Date") + 
+  ylab("Temperature") +  
+  scale_x_date(limit=c(as.Date("2000-01-01"),as.Date("2000-11-30")), date_labels = "%Y %b %d") 
+  
+tps
+
+tpt <- ggplot(twibot, aes(date,temperature, group = depth)) +
+  geom_line(na.rm = TRUE) +
+  geom_point(aes(color=depth)) +
+  xlab("Date") + 
+  ylab("Temperature") +  
+  scale_x_date(limit=c(as.Date("2000-01-01"),as.Date("2000-11-30")), date_labels = "%Y %b %d") 
+
+tpt
+
+#as.data.frame(aggregate(oxygen~date, data=sunbot, mean))$oxygen
+
+# op <- ggplot(sunbot, aes(x=unique(date), y=as.data.frame(aggregate(oxygen~date, data=sunbot, mean))$oxygen)) +
+#   geom_point(na.rm = TRUE)
+#   #geom_line(aes(y=bot0$oxygen), color = "blue", na.rm = TRUE) + 
+#   xlab("Date") +
+#   scale_x_date(limit=c(as.Date("2000-01-01"),as.Date("2000-07-01")),
+#                 date_labels = "%Y %b %d")
+
+  
+ops <- ggplot(sunbot, aes(date,oxygen, group = depth)) +
+    geom_line(na.rm = TRUE) +
+    geom_point(aes(color=depth)) +
+    xlab("Date") + 
+    ylab("OXygen mL/L") +  
+    scale_x_date(limit=c(as.Date("2000-01-01"),as.Date("2000-11-30")), date_labels = "%Y %b %d") 
+  
+ops
+
+opt <- ggplot(twibot, aes(date,oxygen, group = depth)) +
+  geom_line(na.rm = TRUE) +
+  geom_point(aes(color=depth)) +
+  xlab("Date") + 
+  ylab("OXygen mL/L") +  
+  scale_x_date(limit=c(as.Date("2000-01-01"),as.Date("2000-11-30")), date_labels = "%Y %b %d") 
+
+opt
