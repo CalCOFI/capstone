1eee5d71e3de74858a0b81e9febe68d6a48647b8
dcoletta9 <dcoletta@ucsb.edu>
2022-05-25 10:48:55 -0700

    Connect points with dashed line

--- scripts/drafts/time_series.R
+++ scripts/drafts/time_series.R
@@ -21,13 +21,13 @@ x
 tfull <- x %>% 
   ggplot(aes(x=date, y=temperature, group=depth_fac, color=depth_fac,shape=as.factor(quarter))) +
   geom_point(na.rm=T) +
-  geom_smooth(method="loess", se=F) +
+  geom_line(linetype='dashed') +
   labs(title = "Temperature over Time", x = "Date", y = "Temperature (°C)",color="Depth Range (m)", shape = "Quarter") +
   scale_shape_discrete(name="Quarter",
                         breaks=c("1", "2", "3","4"),
-                        labels=c("Winter", "Spring", "Summer","Fall"))
-  
-#scale_x_date(limit=c(as.Date("2000-01-01"),as.Date("2004-11-30")), date_labels = "%Y %b %d")
+                        labels=c("Winter", "Spring", "Summer","Fall")) +
+  scale_x_date(limit=c(as.Date("2000-01-01"),as.Date("2004-11-30")), date_labels = "%Y %b %d")+ 
+  ylim(NA,18)
 
 tfull
 
@@ -41,15 +41,19 @@ y<-mutate(y,ID = as.numeric(rownames(y)))
 ofull <- y %>% 
   ggplot(aes(x=date, y=oxygen, group=depth_fac, color=depth_fac, shape=as.factor(quarter))) +
   geom_point(na.rm=T) +
-  geom_smooth(method="loess", se=F) +
-  labs(title = "Temperature over Time", x = "Date", y = "Oxygen",color="Depth Range (m)", shape = "Quarter") +
+  geom_line(linetype='dashed') +
+  labs(title = "Oxygen Concentration over Time", x = "Date", y = "Oxygen mL/L",color="Depth Range (m)", shape = "Quarter") +
   scale_shape_discrete(name="Quarter",
                        breaks=c("1", "2", "3","4"),
-                       labels=c("Winter", "Spring", "Summer","Fall")) 
+                       labels=c("Winter", "Spring", "Summer","Fall")) +
+  scale_x_date(limit=c(as.Date("2000-01-01"),as.Date("2004-11-30")), date_labels = "%Y %b %d") +
+  ylim(1,NA)
    
 #scale_x_date(limit=c(as.Date("2000-01-01"),as.Date("2004-11-30")), date_labels = "%Y %b %d")
   
 ofull
 
-#geom_errorbar(aes(ymin=temperature-sd, ymax = temperature+sd, width = .1))
-#inputs are parameters(oxygen and temperature) and date (start and end)
+make_ts_plot <- function(date_min, date_max){
+  
+}
+#make_ts_plot('1983-02-01', '2005-06-01')
