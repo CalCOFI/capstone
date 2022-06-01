e66a9b555245d6388009f65bc8b85b18c4cfb588
dcoletta9 <dcoletta@ucsb.edu>
2022-06-01 14:26:22 -0700

    Functionalize time series and inplement to page functions
    I functionalized the time series plots for temperature and oxygen concentration with number of depth ranges as inputs, as well as start date and end date for the displayed plot

--- scripts/drafts/time_series.R
+++ scripts/drafts/time_series.R
@@ -8,52 +8,52 @@ library(dplyr)
 library(hrbrthemes)
 load("data/processed/bottle.RData")
 
-bottle <- bottle %>% mutate(depth_fac = cut(depth, c(-.01, 50, 100, 200,500)))
-
-
-x<-aggregate(list(bottle$temperature,bottle$date), list(bottle$year,bottle$quarter,bottle$depth_fac), FUN=median, na.rm=T)
-colnames(x) <- c('year', 'quarter','depth_fac','temperature','date')
-x <- x[order(x$year, x$quarter),]
-rownames(x)<-1:nrow(x)
-#x<-mutate(x,ID = as.numeric(rownames(x)))
-x
-
-tfull <- x %>% 
-  ggplot(aes(x=date, y=temperature, group=depth_fac, color=depth_fac,shape=as.factor(quarter))) +
-  geom_point(na.rm=T) +
-  geom_line(linetype='dashed') +
-  labs(title = "Temperature over Time", x = "Date", y = "Temperature (°C)",color="Depth Range (m)", shape = "Quarter") +
-  scale_shape_discrete(name="Quarter",
-                        breaks=c("1", "2", "3","4"),
-                        labels=c("Winter", "Spring", "Summer","Fall")) +
-  scale_x_date(limit=c(as.Date("2000-01-01"),as.Date("2004-11-30")), date_labels = "%Y %b %d")+ 
-  ylim(NA,18)
-
-tfull
-
-y<-aggregate(list(bottle$oxygen, bottle$date), list(bottle$year,bottle$quarter,bottle$depth_fac), FUN=median, na.rm=T)
-colnames(y) <- c('year','quarter','depth_fac','oxygen','date')
-y <- y[order(y$year, y$quarter),]
-rownames(y)<-1:nrow(y)
-y<-mutate(y,ID = as.numeric(rownames(y)))
-
-
-ofull <- y %>% 
-  ggplot(aes(x=date, y=oxygen, group=depth_fac, color=depth_fac, shape=as.factor(quarter))) +
-  geom_point(na.rm=T) +
-  geom_line(linetype='dashed') +
-  labs(title = "Oxygen Concentration over Time", x = "Date", y = "Oxygen mL/L",color="Depth Range (m)", shape = "Quarter") +
-  scale_shape_discrete(name="Quarter",
-                       breaks=c("1", "2", "3","4"),
-                       labels=c("Winter", "Spring", "Summer","Fall")) +
-  scale_x_date(limit=c(as.Date("2000-01-01"),as.Date("2004-11-30")), date_labels = "%Y %b %d") +
-  ylim(1,NA)
-   
-#scale_x_date(limit=c(as.Date("2000-01-01"),as.Date("2004-11-30")), date_labels = "%Y %b %d")
+temp_ts_plot <- function(n_ranges, date_min, date_max){
+  bottle <- bottle %>% 
+    filter(depth>=0 & depth<=500) %>%
+    mutate(depth_fac = cut(depth, n_ranges))
   
-ofull
+  x <- aggregate(list(bottle$temperature,bottle$date), list(bottle$year,bottle$quarter,bottle$depth_fac), FUN=median, na.rm=T)
+  colnames(x) <- c('year', 'quarter','depth_fac','temperature','date')
+  x <- x[order(x$year, x$quarter),]
+  rownames(x)<-1:nrow(x)
+  
+  x %>% 
+    ggplot(aes(x=date, y=temperature, group=depth_fac, color=depth_fac, shape=as.factor(quarter))) +
+    geom_point(na.rm=T) +
+    scale_y_continuous(limits=c(NA,24), expand = c(0, 0)) +
+    geom_line(linetype='dashed') +
+    labs(title = "Temperature over Time", x = "Date", y = "Temperature (°C)", color = "Depth Range (m)", shape = "Quarter") +
+    scale_shape_discrete(name="Quarter",
+                         breaks=c("1", "2", "3","4"),
+                         labels=c("Winter", "Spring", "Summer","Fall")) +
+    scale_x_date(limit=c(as.Date(date_min),as.Date(date_max)), date_labels = "%Y %b %d") 
+    
+}
 
-make_ts_plot <- function(date_min, date_max){
+temp_ts_plot(6,'1971-06-14', '2004-01-15')
+
+oxy_ts_plot <- function(n_ranges, date_min, date_max){
+  bottle <- bottle %>% 
+    filter(depth>=0 & depth<=500) %>%
+      mutate(depth_fac = cut(depth, n_ranges))
+  
+  x <- aggregate(list(bottle$oxygen,bottle$date), list(bottle$year,bottle$quarter,bottle$depth_fac), FUN=median, na.rm=T)
+  colnames(x) <- c('year', 'quarter','depth_fac','oxygen','date')
+  x <- x[order(x$year, x$quarter),]
+  rownames(x)<-1:nrow(x)
+  
+  x %>% 
+    ggplot(aes(x=date, y=oxygen, group=depth_fac, color=depth_fac, shape=as.factor(quarter))) +
+    geom_point(na.rm=T) +
+    scale_y_continuous(limits=c(NA,7), expand = c(0, 0)) +
+    geom_line(linetype='dashed') +
+    labs(title = "Oxygen over Time", x = "Date", y = "Temperature (°C)", color = "Depth Range (m)", shape = "Quarter") +
+    scale_shape_discrete(name="Quarter",
+                         breaks=c("1", "2", "3","4"),
+                         labels=c("Winter", "Spring", "Summer","Fall")) +
+    scale_x_date(limit=c(as.Date(date_min),as.Date(date_max)), date_labels = "%Y %b %d") 
   
 }
-#make_ts_plot('1983-02-01', '2005-06-01')
+
+oxy_ts_plot(5,'1970-06-14', '2020-01-15')
--- scripts/shiny/spatial-page/page_functions.R
+++ scripts/shiny/spatial-page/page_functions.R
@@ -550,6 +550,51 @@ depth_avg_plot <- function(start_input, end_input, date_input, station_input, li
   }
 }
 
+temp_ts_plot <- function(n_ranges, date_min, date_max){
+  bottle <- bottle %>% 
+    filter(depth>=0 & depth<=500) %>%
+    mutate(depth_fac = cut(depth, n_ranges))
+  
+  x <- aggregate(list(bottle$temperature,bottle$date), list(bottle$year,bottle$quarter,bottle$depth_fac), FUN=median, na.rm=T)
+  colnames(x) <- c('year', 'quarter','depth_fac','temperature','date')
+  x <- x[order(x$year, x$quarter),]
+  rownames(x)<-1:nrow(x)
+  
+  x %>% 
+    ggplot(aes(x=date, y=temperature, group=depth_fac, color=depth_fac, shape=as.factor(quarter))) +
+    geom_point(na.rm=T) +
+    scale_y_continuous(limits=c(NA,24), expand = c(0, 0)) +
+    geom_line(linetype='dashed') +
+    labs(title = "Temperature over Time", x = "Date", y = "Temperature (°C)", color = "Depth Range (m)", shape = "Quarter") +
+    scale_shape_discrete(name="Quarter",
+                         breaks=c("1", "2", "3","4"),
+                         labels=c("Winter", "Spring", "Summer","Fall")) +
+    scale_x_date(limit=c(as.Date(date_min),as.Date(date_max)), date_labels = "%Y %b %d") 
+  
+}
+
+oxy_ts_plot <- function(n_ranges, date_min, date_max){
+  bottle <- bottle %>% 
+    filter(depth>=0 & depth<=500) %>%
+    mutate(depth_fac = cut(depth, n_ranges))
+  
+  x <- aggregate(list(bottle$oxygen,bottle$date), list(bottle$year,bottle$quarter,bottle$depth_fac), FUN=median, na.rm=T)
+  colnames(x) <- c('year', 'quarter','depth_fac','oxygen','date')
+  x <- x[order(x$year, x$quarter),]
+  rownames(x)<-1:nrow(x)
+  
+  x %>% 
+    ggplot(aes(x=date, y=oxygen, group=depth_fac, color=depth_fac, shape=as.factor(quarter))) +
+    geom_point(na.rm=T) +
+    scale_y_continuous(limits=c(NA,7), expand = c(0, 0)) +
+    geom_line(linetype='dashed') +
+    labs(title = "Oxygen over Time", x = "Date", y = "Temperature (°C)", color = "Depth Range (m)", shape = "Quarter") +
+    scale_shape_discrete(name="Quarter",
+                         breaks=c("1", "2", "3","4"),
+                         labels=c("Winter", "Spring", "Summer","Fall")) +
+    scale_x_date(limit=c(as.Date(date_min),as.Date(date_max)), date_labels = "%Y %b %d") 
+  
+}
 
 save(
   list = ls(),
