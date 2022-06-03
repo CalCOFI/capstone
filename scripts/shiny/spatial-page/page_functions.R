c9c13a3eecd34f0ada7286c2f9ebfcd335cd4aa3
dcoletta9 <dcoletta@ucsb.edu>
2022-06-03 16:27:38 -0700

    Time series adjustments and additions
    Added time series plot for chlorophyll and salinity. Added titles for all plots and allowed to select entirety of depth range (0-500 m) as a selection to view.

--- scripts/shiny/spatial-page/page_functions.R
+++ scripts/shiny/spatial-page/page_functions.R
@@ -623,9 +623,16 @@ depth_avg_plot <- function(start_input, end_input, date_input, station_input, li
 }
 
 temp_ts_plot <- function(n_ranges, date_min, date_max){
-  bottle <- bottle %>% 
-    filter(depth>=0 & depth<=500) %>%
-    mutate(depth_fac = cut(depth, n_ranges))
+  if (n_ranges > 1){
+    bottle <- bottle %>% 
+      filter(depth>=0 & depth<=500) %>%
+      mutate(depth_fac = cut(depth, n_ranges))
+  }
+  else {
+    bottle <- bottle %>% 
+      filter(depth>=0 & depth<=500) %>%
+      mutate(depth_fac= rep("[0,500]",length(year)))
+  }
   
   x <- aggregate(list(bottle$temperature,bottle$date), list(bottle$year,bottle$quarter,bottle$depth_fac), FUN=median, na.rm=T)
   colnames(x) <- c('year', 'quarter','depth_fac','temperature','date')
@@ -636,19 +643,26 @@ temp_ts_plot <- function(n_ranges, date_min, date_max){
     ggplot(aes(x=date, y=temperature, group=depth_fac, color=depth_fac, shape=as.factor(quarter))) +
     geom_point(na.rm=T) +
     geom_line(linetype='dashed') +
-    labs(x = "Date", y = "Temperature (°C)", color = "Depth Range (m)", shape = "Quarter") +
+    labs(title = "Median Temperature Across All Stations Over Time", x = "Date", y = "Temperature (°C)", color = "Depth Range (m)", shape = "Quarter") +
     scale_shape_discrete(name="Quarter",
                          breaks=c("1", "2", "3","4"),
                          labels=c("Winter", "Spring", "Summer","Fall")) +
     scale_x_date(limit=c(as.Date(date_min),as.Date(date_max)), date_labels = "%Y %b %d", breaks = scales::breaks_pretty(7)) +
     scale_y_continuous(limits=c(NA,NA),expand = c(0.1, 0.1)) +
-    theme_bw()  
+    theme_bw() 
 }
 
 oxy_ts_plot <- function(n_ranges, date_min, date_max){
-  bottle <- bottle %>% 
-    filter(depth>=0 & depth<=500) %>%
-    mutate(depth_fac = cut(depth, n_ranges))
+  if (n_ranges > 1){
+    bottle <- bottle %>% 
+      filter(depth>=0 & depth<=500) %>%
+      mutate(depth_fac = cut(depth, n_ranges))
+  }
+  else {
+    bottle <- bottle %>% 
+      filter(depth>=0 & depth<=500) %>%
+      mutate(depth_fac= rep("[0,500]",length(year)))
+  }
   
   x <- aggregate(list(bottle$oxygen,bottle$date), list(bottle$year,bottle$quarter,bottle$depth_fac), FUN=median, na.rm=T)
   colnames(x) <- c('year', 'quarter','depth_fac','oxygen','date')
@@ -659,7 +673,7 @@ oxy_ts_plot <- function(n_ranges, date_min, date_max){
     ggplot(aes(x=date, y=oxygen, group=depth_fac, color=depth_fac, shape=as.factor(quarter))) +
     geom_point(na.rm=T) +
     geom_line(linetype='dashed') +
-    labs(title = "", x = "Date", y = "Oxygen (mL/L)", color = "Depth Range (m)", shape = "Quarter") +
+    labs(title = title = "Median Oxygen Across All Stations Over Time", x = "Date", y = "Oxygen (mL/L)", color = "Depth Range (m)", shape = "Quarter") +
     scale_shape_discrete(name="Quarter",
                          breaks=c("1", "2", "3","4"),
                          labels=c("Winter", "Spring", "Summer","Fall")) +
@@ -668,7 +682,69 @@ oxy_ts_plot <- function(n_ranges, date_min, date_max){
     theme_bw() 
   
 }
-oxy_ts_plot(5, '1970-01-15', '2020-01-15')
+
+cho_ts_plot <- function(n_ranges, date_min, date_max){
+  if (n_ranges > 1){
+    bottle <- bottle %>% 
+      filter(depth>=0 & depth<=500) %>%
+      mutate(depth_fac = cut(depth, n_ranges))
+  }
+  else {
+    bottle <- bottle %>% 
+      filter(depth>=0 & depth<=500) %>%
+      mutate(depth_fac= rep("[0,500]",length(year)))
+  }
+  
+  x <- aggregate(list(bottle$chlorophyll,bottle$date), list(bottle$year,bottle$quarter,bottle$depth_fac), FUN=median, na.rm=T)
+  colnames(x) <- c('year', 'quarter','depth_fac','chlorophyll','date')
+  x <- x[order(x$year, x$quarter),]
+  rownames(x)<-1:nrow(x)
+  
+  x %>% 
+    ggplot(aes(x=date, y=chlorophyll, group=depth_fac, color=depth_fac, shape=as.factor(quarter))) +
+    geom_point(na.rm=T) +
+    geom_line(linetype='dashed') +
+    labs(title = "Median Chlorophyll Across All Stations Over Time", x = "Date", y = "Chlorophyll (mg/L)", color = "Depth Range (m)", shape = "Quarter") +
+    scale_shape_discrete(name="Quarter",
+                         breaks=c("1", "2", "3","4"),
+                         labels=c("Winter", "Spring", "Summer","Fall")) +
+    scale_x_date(limit=c(as.Date(date_min),as.Date(date_max)), date_labels = "%Y %b %d", breaks = scales::breaks_pretty(7)) +
+    scale_y_continuous(limits=c(0,0.8)) +
+    theme_bw() 
+  
+}
+
+sal_ts_plot <- function(n_ranges, date_min, date_max){
+  if (n_ranges > 1){
+    bottle <- bottle %>% 
+      filter(depth>=0 & depth<=500) %>%
+      mutate(depth_fac = cut(depth, n_ranges))
+  }
+  else {
+    bottle <- bottle %>% 
+      filter(depth>=0 & depth<=500) %>%
+      mutate(depth_fac= rep("[0,500]",length(year)))
+  }
+  
+  x <- aggregate(list(bottle$salinity,bottle$date), list(bottle$year,bottle$quarter,bottle$depth_fac), FUN=median, na.rm=T)
+  colnames(x) <- c('year', 'quarter','depth_fac','salinity','date')
+  x <- x[order(x$year, x$quarter),]
+  rownames(x)<-1:nrow(x)
+  
+  x %>% 
+    ggplot(aes(x=date, y=salinity, group=depth_fac, color=depth_fac, shape=as.factor(quarter))) +
+    geom_point(na.rm=T) +
+    geom_line(linetype='dashed') +
+    labs(title = "Median Salinity Across All Stations Over Time", x = "Date", y = "Salinity (Practical Salinity Scale)", color = "Depth Range (m)", shape = "Quarter") +
+    scale_shape_discrete(name="Quarter",
+                         breaks=c("1", "2", "3","4"),
+                         labels=c("Winter", "Spring", "Summer","Fall")) +
+    scale_x_date(limit=c(as.Date(date_min),as.Date(date_max)), date_labels = "%Y %b %d", breaks = scales::breaks_pretty(7)) +
+    scale_y_continuous(limits=c(33,36)) +
+    theme_bw() 
+}
+
+#oxy_ts_plot(1,'2000-01-15','2010-05-20')
 
 save(
   list = ls(),
